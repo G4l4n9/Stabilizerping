920377796f9f853f0400c6814c61d8dc04301214
G4l4n9 <64618623+G4l4n9@users.noreply.github.com>
2020-05-01 06:24:52 +0700

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,1 +1,35 @@
 
+#!/system/bin/sh
+clear
+echo ""
+sleep 1
+echo "Script dibuat oleh GALANG36"
+echo "[----------------------------------]"
+sleep 1
+echo "Script Memperstabil Koneksi Jaringan"
+echo "[----------------------------------]"
+echo ""
+sleep 1
+echo "Ketik [y] utk mempercepat koneksi jaringan atau ketik [n] untuk keluar dari script"
+sleep 1
+echo ""
+read -p "Masukkan disini = " bro
+
+if [ $bro = y ] || [ $bro = y ] 
+
+then 
+clear 
+figlet "Tunggu ya"
+sleep 2
+echo "jika keluar tulisan ping angka sekian jangan di keluarkan aplikasinya tapi tekan tombol home"
+sleep 3
+ping 8.8.8.8
+fi
+
+if [ $bro = n ] || [ $bro = n ]
+
+then
+clear
+sleep 2
+echo "terima kasih yang sudah pakai script ini dan jika ada kesalahan komen aja di youtube channel Galang 36"
+fi
